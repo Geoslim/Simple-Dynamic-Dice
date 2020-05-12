c73da672904ea2a121328f348ea426933b2066bb
Edomwande Ikponmwosa George <iedomwande@yahoo.com>
2020-05-13 00:22:46 +0100

    Dice rolling with prompts to show number rolled

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.redAccent,
         appBar: AppBar(
-          title: Text('Dicee'),
+          title: Center(child: Text('Dynamic Dice')),
           backgroundColor: Colors.red,
         ),
         body: DicePage(),
@@ -15,9 +17,109 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = Random().nextInt(6) + 1;
+  int rightDiceNumber = Random().nextInt(6) + 1;
+  dynamic numberRolled = "";
+  dynamic add = 12;
+
+  void rollDice() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+      add = leftDiceNumber + rightDiceNumber;
+      numberRolled = "You rolled a $add";
+    });
+  }
+
+  Widget _getIcon() {
+    if (add >= 9)
+      return Icon(
+        Icons.tag_faces,
+        color: Colors.white,
+        size: 50.0,
+      );
+    else if (add > 4)
+      return Icon(
+        Icons.face,
+        color: Colors.white,
+        size: 50.0,
+      );
+    else if (add <= 4)
+      return Icon(
+        Icons.thumb_down,
+        color: Colors.white,
+        size: 50.0,
+      );
+    else
+      return Icon(Icons.wb_sunny);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Stack(
+      children: <Widget>[
+        Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Text(
+                '$numberRolled',
+                style: TextStyle(
+                  fontSize: 60.0,
+                  color: Colors.white,
+                ),
+              ),
+              _getIcon(),
+            ],
+          ),
+        ),
+        Center(
+          child: Row(
+            children: <Widget>[
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Image.asset(
+                    'images/dice$leftDiceNumber.png',
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Image.asset(
+                    'images/dice$rightDiceNumber.png',
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: <Widget>[
+            FlatButton(
+              padding: EdgeInsets.all(18.0),
+              color: Colors.amberAccent,
+              child: Text(
+                "Roll Yo!!",
+                style: TextStyle(fontSize: 20.0),
+              ),
+              splashColor: Colors.white,
+              onPressed: () {
+                rollDice();
+              },
+            ),
+          ],
+        ),
+      ],
+    );
   }
 }
